683332ec670e27fc8d1e48b98515e418b76cdce0
Emroze <emrozesd@users.noreply.github.com>
2018-12-21 21:23:52 -0800

    initial game files added

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//Four Characters to Choose From to Start
+//Each have a different number of HP
+//Clicked character becomes your character
+//The rest of the characters move to the "enemy" part of the screen
+//The enemy clicked next moves to the defender area 
+//Enemy can be fought by using the "attack button"
+//Text is displayed with damage attacked/damage attacked back
+//Number is subtracted from player and enemy's HP
+//User character's attack power increases with each attack
+//Enemy's remains the same 
+//If player defeats enemy, player can choose another enemy to fight
+//If player hits the attack button with no enemy, no enemy here is displayed 
+//Player loses when HP <= 0
+//Player can restart game completely by hitting the restart button 
